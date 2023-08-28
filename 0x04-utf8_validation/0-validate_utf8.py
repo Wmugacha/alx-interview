51c2f3a43622446ebf5439d7a4bf4d9396833402
Wmugacha <wilfredmugacha@gmail.com>
2023-08-28 02:46:53 +0000

    utf-8 encoding

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -8,43 +8,42 @@ def validUTF8(data):
     """method that determines if a given data set
     represents a valid UTF-8 encoding"""
 
-    i = 0
-    while i < len(data):
-        integer = data[i]
+    # Variable to keep track of the number of bytes
+    remaining_bytes = 0
 
-        if integer < 0 or integer > 255:
-            return False  # Each integer should be a valid byte (0 to 255)
+    # Masks for checking valid byte patterns (10xxxxxx)
+    continuation_mask_1 = 1 << 7
+    continuation_mask_2 = 1 << 6
 
-        byte = bytes([integer])[0]  # Convert integer to bytes
+    for byte_value in data:
 
-        if byte & 0x80 == 0x00:
-            # ASCII character
-            i += 1
-        elif byte & 0xE0 == 0xC0:
-            # Two-byte character
-            # Validate continuation byte
-            if i + 1 >= len(data) or data[i + 1] & 0xC0 != 0x80:
+        byte_mask = 1 << 7
+
+        if remaining_bytes == 0:
+            # Determine the number of bytes the UTF-8 character will have
+            while byte_mask & byte_value:
+                remaining_bytes += 1
+                byte_mask = byte_mask >> 1
+
+            # If the byte count did not increase, it's a 1-byte character
+            if remaining_bytes == 0:
+                continue
+
+            # UTF-8 characters can be 1 to 4 bytes long
+            if remaining_bytes == 1 or remaining_bytes > 4:
                 return False
-            i += 2
-        elif byte & 0xF0 == 0xE0:
-            # Three-byte character
-            # Validate continuation bytes
-            if i + 2 >= len(data):
-                return False
-            for j in range(i + 1, i + 3):
-                if j >= len(data) or data[j] & 0xC0 != 0x80:
-                    return False
-            i += 3
-        elif byte & 0xF8 == 0xF0:
-            # Four-byte character
-            # Validate continuation bytes
-            if i + 3 >= len(data):
-                return False
-            for j in range(i + 1, i + 4):
-                if j >= len(data) or data[j] & 0xC0 != 0x80:
-                    return False
-            i += 4
+
         else:
-            return False  # Invalid byte pattern
+            # Continuation bytes should start with 10
+            if not (byte_value & continuation_mask_1 and not
+                    (byte_value & continuation_mask_2)):
+                return False
 
-    return True
+        # Decrement the byte count as we process each byte of the character
+        remaining_bytes -= 1
+
+    # All characters were verified correctly with their proper byte count
+    if remaining_bytes == 0:
+        return True
+
+    return False
