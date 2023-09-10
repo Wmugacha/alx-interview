6642f4f1d703450f70a46f962b8b8c93c5e39800
Wmugacha <wilfredmugacha@gmail.com>
2023-09-10 17:46:37 +0000

    movie characters

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+const argv = process.argv;
+const filmApiUrl = 'https://swapi-api.hbtn.io/api/films/';
+const movieUrl = `${filmApiUrl}${argv[2]}/`;
+
+const request = require('request');
+
+request(movieUrl, function (error, response, body) {
+  if (error == null) {
+    const movieData = JSON.parse(body);
+    const characters = movieData.characters;
+
+    if (characters && characters.length > 0) {
+      const characterLimit = characters.length;
+      fetchCharacters(0, characters[0], characters, characterLimit);
+    }
+  } else {
+    console.log(error);
+  }
+});
+
+function fetchCharacters(index, characterUrl, characters, limit) {
+  if (index === limit) {
+    return;
+  }
+  request(characterUrl, function (error, response, body) {
+    if (!error) {
+      const characterData = JSON.parse(body);
+      console.log(characterData.name);
+      index++;
+      fetchCharacters(index, characters[index], characters, limit);
+    } else {
+      console.error('Error:', error);
+    }
+  });
+}
---
+++ 0x06-starwars_api/README.md
@@ -0,0 +1,3 @@
+## Project 0x06. Star Wars API
+
+* Script that queries the star wars API
