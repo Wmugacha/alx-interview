ec588c7ed1557d9579d29593987aca9c5e0b8cd4
Wmugacha <wilfredmugacha@gmail.com>
2023-09-27 04:06:30 +0000

    grid island perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Island Perimeter
+"""
+
+
+def island_perimeter(grid):
+    """Function to calculate perimeter of a grid island"""
+
+    if not grid:
+        return 0
+
+    perimeter = 0
+
+    rows, cols = len(grid), len(grid[0])
+
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 1:
+                perimeter += 4
+
+                if row > 0 and grid[row - 1][col] == 1:
+                    perimeter -= 1
+                if row < rows - 1 and grid[row + 1][col] == 1:
+                    perimeter -= 1
+                if col > 0 and grid[row][col - 1] == 1:
+                    perimeter -= 1
+                if col < cols - 1 and grid[row][col + 1] == 1:
+                    perimeter -= 1
+
+    return perimeter
---
+++ 0x09-island_perimeter/README.md
@@ -0,0 +1,3 @@
+## Project 0x09. Island Perimeter
+
+* Create a function def island_perimeter(grid): that returns the perimeter of the island described in grid
